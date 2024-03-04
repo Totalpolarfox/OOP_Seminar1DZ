995f7e8466a58f6e1bb2467e413ff1b37d304460
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-04 19:52:12 +0300

    Added getter ans setter in class AnimalFeed

--- AnimalFeed.java
+++ AnimalFeed.java
@@ -3,5 +3,18 @@ public class AnimalFeed extends Product {
     private String animal;
     private String typeFeed;
     
+    public String getAnimal() {
+        return animal;
+    }
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+    
+    public String getTypeFeed() {
+        return typeFeed;
+    }
+    public void setTypeFeed(String typeFeed) {
+        this.typeFeed = typeFeed;
+    }
 
 }
