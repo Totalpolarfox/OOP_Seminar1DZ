7d5187f03f534b9ac37cf7c1913bda0b9b3d41be
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-04 20:28:42 +0300

    Added data in class Program

--- Program.java
+++ Program.java
@@ -3,6 +3,25 @@
 
 public class Program {
     public static void main(String[] args) {
+
+        AnimalFeed animalFeed1 = new AnimalFeed("Royal Canin", "Puppy", 1350, "dog", "dry");
+        AnimalFeed animalFeed2 = new AnimalFeed("AlphaPet", "Satiety", 1700, "dog", "dry");
+        AnimalFeed animalFeed3 = new AnimalFeed("Royal Canin", "Renal", 1100, "cat", "dry");
+        AnimalFeed animalFeed4 = new AnimalFeed("Sheba", "Adult", 950, "dog", "wet");
+        AnimalFeed animalFeed5 = new AnimalFeed("AlphaPet", "Exigent", 1000, ""dog", wet");
+        AnimalFeed animalFeed6 = new AnimalFeed("Sheba", "Sensible", 1200, "cat", "dry");
+        AnimalFeed animalFeed7 = new AnimalFeed("Royal Canin", "Sterelised", 850, "cat", "wet");
+        AnimalFeed animalFeed8 = new AnimalFeed("Felix", "Kitten", 900, "cat", "wet");
+        AnimalFeed animalFeed9 = new AnimalFeed("Purina", "Sterelised", 900, "cat", "dry");
+        AnimalFeed animalFeed10 = new AnimalFeed("Perfect Fit", "OptyRenal", 750, "cat", "dry");
+        AnimalFeed animalFeed11 = new AnimalFeed("Whiskas", "Digestion", 950, "dog", "dry");
+        AnimalFeed animalFeed12 = new AnimalFeed("Purina", "Nutrytion", 1200, "dog", "dry");
+        AnimalFeed animalFeed13 = new AnimalFeed("Purina", "NutrySavoir", 1100, "cat", "wet");
+        AnimalFeed animalFeed14 = new AnimalFeed("Perfect Fit", "Maitenance", 600, "cat", "wet");
+        AnimalFeed animalFeed15 = new AnimalFeed("Felix", "OptySavoir", 800, "dog", "wet");
+        AnimalFeed animalFeed16 = new AnimalFeed("Purina", "Sensetive", 1000, "dog", "wet");
         
+            
+
     }
 }
