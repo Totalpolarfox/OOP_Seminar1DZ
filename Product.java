70f04a9f9e217c043f6267e924aaa680020341f2
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-04 19:30:05 +0300

    Modified class Product

--- Product.java
+++ Product.java
@@ -2,6 +2,79 @@
  * Продукт
  */
 
-public class Product {
+ public class Product {
+    protected String brand;
+    protected String name;
+    protected double price;
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        checkBrand(brand);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        checkName(name);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        checkPrice(price);
+    }
+
+    private void checkBrand(String brand) {
+        if (brand == null || brand.length() < 3) {
+            this.brand = "<BRAND>";
+        } else {
+            this.brand = brand;
+        }
+    }
+
+    private void checkName(String name) {
+        if (name == null || name.length() < 3) {
+            this.name = "<NAME>";
+        } else {
+            this.name = name;
+        }
+    }
+
+    private void checkPrice(double price) {
+        if (price < 100) {
+            this.price = 250;
+        } else {
+            this.price = price;
+        }
+    }
+
+    // конструкторы
+    public Product(String brand, String name, double price) {
+        checkBrand(brand);
+        checkName(name);
+        checkPrice(price);
+    }
+
+    public Product(String name, double price) { // ссылается на предыдущий
+        this("<BRAND>", name, price);
+    }
+
+    public Product(String name) { // ссылается на предыдущий
+        this(name, 250);
+    }
+
+    public Product() { // ссылается на предыдущий
+        this("<NAME>");
+    }
+
+    public String displayInfo() {
+        return String.format("%s - %s - %.2f", brand, name, price);
+    }
 }
