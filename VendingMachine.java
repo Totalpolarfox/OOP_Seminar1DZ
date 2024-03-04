932bf4d3908cc3bda8e2db062aeb62f657152f8c
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-04 20:45:48 +0300

    Added getAnimal method in class VendingMachine

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,6 +1,23 @@
+import java.util.List;
+
 public class VendingMachine {
 
     private List<Product> products;
-    
+
+    public VendingMachine(List<Product> products) {
+        this.products = products;
+    }
+
+    public AnimalFeed getAnimal(String brand, String animal) {
+        for (Product product : products) {
+            if (product instanceof AnimalFeed) {
+                AnimalFeed animalFeed = (AnimalFeed)product;
+                if (animalFeed.getBrand().equals(brand) && animalFeed.getAnimal().equals(animal)) {
+                    return animalFeed;
+                }
+            }
+        }
+        return null;
+    }
 
 }
